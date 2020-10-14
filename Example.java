6e47312d2af4add1b25d1d5ec4595ca6b10a840e
Zayed Alabbad <zaalabbad@gmail.com>
2020-10-14 21:12:25 +0300

    message

--- Example.java
+++ Example.java
@@ -2,4 +2,8 @@ public class Example {
     public static void main(String[] args) {
         System.out.print("Hello");
     }
+
+    public static void sayHello() {
+        System.out.print("Hello");
+    }
 }
